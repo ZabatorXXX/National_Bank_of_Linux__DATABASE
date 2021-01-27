b35625d808cdb36435507d60acf73599bd939d7e
ZabatorXXX <42642927+ZabatorXXX@users.noreply.github.com>
2021-01-27 16:43:07 +0100

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,5 @@
+
+insert into bank_accounts (first_name, last_name, holding) values ('Dave', 'Lukasosako', 6969);
+insert into bank_accounts (first_name, last_name, holding) values ('Steve', 'Blockhead', 45012);
+insert into bank_accounts (first_name, last_name, holding) values ('Joe', 'Mama', 23477);
+insert into bank_accounts (first_name, last_name, holding) values ('Testas', 'Holddingo', 9090110);
